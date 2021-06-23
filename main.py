b850890e8ec69dfb1e1986af825020ba959e8710
Miron <miros.kowalewski@gmail.com>
2021-06-23 19:21:41 +0200

    nowa_funkcja3

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def nowa_funkcja():
 def nowa_funkcja2():
     print("Funkcja nowa");
 
+def nowa_funkcja3():
+    print("Funkcja nowa");
+
+
 def start_game():
     random_number = int(random.randint(1, 10))
     print("Hello traveler! Welcome to the game of guesses!")
